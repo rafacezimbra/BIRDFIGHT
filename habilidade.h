e7434e92389a72ee27b7aba96f7f2ef71d8efd8c
LeticiadaMata <letidamata25@gmail.com>
2024-09-14 19:19:58 -0300

    Update habilidade.h
    habilidades ajeitadas
    obs: sem efeitos associados

--- habilidade.h
+++ habilidade.h
@@ -25,275 +25,178 @@ int dano;
 //TESTE DE FUNCAO
 // Função para inicializar uma habilidade
 void criarHabilidades(tp_hab hab[]) {
-  
-    //bicada
+  //HABILIDADE 1:
     strcpy(hab[0].nome, "Bicada");
     strcpy(hab[0].tipo, "nenhum");
     hab[0].dano = 30; 
+    
+    strcpy(hab[1].nome, "Chute");
+    strcpy(hab[1].tipo, "nenhum");
+    hab[1].dano = 30;
+    
+    strcpy(hab[2].nome, "Soco");
+    strcpy(hab[2].tipo, "nenhum");
+    hab[2].dano = 30;
+
+  //HABILIDADE 2:
+    strcpy(hab[3].nome, "Choque");
+    strcpy(hab[3].tipo, "eletrico");
+    hab[3].dano = 40;
+    
+    strcpy(hab[4].nome, "Amaldicoar");
+    strcpy(hab[4].tipo, "sombrio");
+    hab[4].dano = 35;
+    
+    strcpy(hab[5].nome, "Encharcar");
+    strcpy(hab[5].tipo, "agua");
+    hab[5].dano = 35;
+    
+    strcpy(hab[6].nome, "Brigar");
+    strcpy(hab[6].tipo, "lutador");
+    hab[6].dano = 40;
+    
+    strcpy(hab[7].nome, "Sambar");
+    strcpy(hab[7].tipo, "dancarino");
+    hab[7].dano = 40;
+    
+    strcpy(hab[8].nome, "Semear");
+    strcpy(hab[8].tipo, "planta");
+    hab[8].dano = 40;
+    
+    strcpy(hab[9].nome, "Rolar");
+    strcpy(hab[9].tipo, "normal");
+    hab[9].dano = 40;
+    
+  //HABILIDADE 3:
+    strcpy(hab[10].nome, "Eletrizar");
+    strcpy(hab[10].tipo, "eletrico");
+    hab[10].dano = 0;
+
+    strcpy(hab[11].nome, "Petrificar");
+    strcpy(hab[11].tipo, "sombrio");
+    hab[11].dano = 0; 
+
+    strcpy(hab[12].nome, "Relaxar");
+    strcpy(hab[12].tipo, "agua");
+    hab[12].dano = 0; 
+
+    strcpy(hab[13].nome, "Tocar Berimbau");
+    strcpy(hab[13].tipo, "dancarino");
+    hab[13].dano = 0; 
+
+    strcpy(hab[14].nome, "Grito de Guerra");
+    strcpy(hab[14].tipo, "lutador");
+    hab[14].dano = 0;
+    
+    strcpy(hab[15].nome, "Botar Saia");
+    strcpy(hab[15].tipo, "dancarino");
+    hab[15].dano = 0;
+    
+    strcpy(hab[16].nome, "Chorar");
+    strcpy(hab[16].tipo, "normal");
+    hab[16].dano = 0;
+    
+    strcpy(hab[17].nome, "Hibernar");
+    strcpy(hab[17].tipo, "normal");
+    hab[17].dano = 0;
+    
+    strcpy(hab[18].nome, "Golpe Baixo");
+    strcpy(hab[18].tipo, "lutador");
+    hab[18].dano = 25;
+    
+    strcpy(hab[19].nome, "Enrolar");
+    strcpy(hab[19].tipo, "planta");
+    hab[19].dano = 0;
+    
+    strcpy(hab[20].nome, "Festa em Ipanema");
+    strcpy(hab[20].tipo, "dancarino");
+    hab[20].dano = 0;
+    
+    strcpy(hab[21].nome, "Noite Chuvosa");
+    strcpy(hab[21].tipo, "Sombrio");
+    hab[21].dano = 0;
+    
+    strcpy(hab[22].nome, "Acelerar!");
+    strcpy(hab[22].tipo, "eletrico");
+    hab[22].dano = 0;
+    
+    strcpy(hab[23].nome, "Bolo de Morango");
+    strcpy(hab[23].tipo, "normal");
+    hab[23].dano = 0;
+    
+    strcpy(hab[24].nome, "Sorrir e Acenar");
+    strcpy(hab[24].tipo, "dancarino");
+    hab[24].dano = 0;
+    
+    strcpy(hab[25].nome, "Camuflagem");
+    strcpy(hab[25].tipo, "planta");
+    hab[25].dano = 0;
+
+  //HABILIDADE 4:
+    strcpy(hab[26].nome, "Bombardeio Eletrico");
+    strcpy(hab[26].tipo, "eletrico");
+    hab[26].dano = 80;
+    
+    strcpy(hab[27].nome, "Paralizar");
+    strcpy(hab[27].tipo, "sombrio");
+    hab[27].dano = 50;
+    
+    strcpy(hab[28].nome, "Agua Quente");
+    strcpy(hab[28].tipo, "agua");
+    hab[28].dano = 55;
+    
+    strcpy(hab[29].nome, "Meia Lua");
+    strcpy(hab[29].tipo, "agua");
+    hab[29].dano = 55;
+    
+    strcpy(hab[30].nome, "Avancar com Escudo");
+    strcpy(hab[30].tipo, "lutador");
+    hab[30].dano = 60;
+    
+    strcpy(hab[31].nome, "Pintinho Amarelinho");
+    strcpy(hab[31].tipo, "dancarino");
+    hab[31].dano = 0;
+    
+    strcpy(hab[32].nome, "Tacada");
+    strcpy(hab[32].tipo, "normal");
+    hab[32].dano = 60;
+    
+    strcpy(hab[33].nome, "Extincao");
+    strcpy(hab[33].tipo, "normal");
+    hab[33].dano = 999;
+    
+    strcpy(hab[34].nome, "Rolada");
+    strcpy(hab[34].tipo, "normal");
+    hab[34].dano = 65;
+    
+    strcpy(hab[35].nome, "Ta Dando Onda");
+    strcpy(hab[35].tipo, "planta");
+    hab[35].dano = 55;
+    
+    strcpy(hab[36].nome, "Carnaval");
+    strcpy(hab[36].tipo, "dancarino");
+    hab[36].dano = 85;
+    
+    strcpy(hab[37].nome, "Evanescence");
+    strcpy(hab[37].tipo, "sombrio");
+    hab[37].dano = 35;
+    
+    strcpy(hab[38].nome, "Beep Beep!");
+    strcpy(hab[38].tipo, "planta");
+    hab[38].dano = 50;
+    
+    strcpy(hab[39].nome, "E la vamos nos!");
+    strcpy(hab[39].tipo, "planta");
+    hab[39].dano = 55;
+    
+    strcpy(hab[40].nome, "boom?");
+    strcpy(hab[40].tipo, "agua");
+    hab[40].dano = 90;
+    
+    strcpy(hab[41].nome, "Explosao de Kiwi");
+    strcpy(hab[41].tipo, "planta");
+    hab[41].dano = 60;
 }
 
 
-
-
-
-
-// chute
-tp_hab chute;
-chute.nome = "Chute";
-chute.tipo = "nenhum";
-chute.dano = 30;
-
-// soco
-tp_hab soco;
-soco.nome = "soco";
-soco.tipo = "nenhum";
-soco.dano = 30;
-
-// HABILIDADE 2
-// habilidades na segunda posicao geralmente sao ataques do tipo do pokemon
-//choque
-tp_hab choque;
-choque.nome = "Choque";
-choque.tipo = "eletrico";
-choque.dano = 40;
-
-//amaldicoar
-tp_hab amaldicoar;
-amaldicoar.nome = "Amaldicoar";
-amaldicoar.tipo = "sombrio";
-amaldicoar.dano = 35;
-
-//encharcar
-tp_hab encharcar;
-encharcar.nome = "Encharcar";
-encharcar.tipo = "agua";
-encharcar.dano = 35;
-
-//brigar
-tp_hab brigar;
-brigar.nome = "Brigar";
-brigar.tipo = "lutador";
-brigar.dano = 40;
-
-//sambar
-tp_hab sambar;
-sambar.nome = "Sambar";
-sambar.tipo = "dancarino";
-sambar.dano = 40;
-
-//semear
-tp_hab semear;
-semear.nome = "Semear";
-semear.tipo = "planta";
-semear.dano = 40;
-
-//rolar
-tp_hab rolar;
-rolar.nome = "Rolar";
-rolar.tipo = "normal";
-rolar.dano = 40;
-
-// HABILIDADE 3
-// Habilidades da posicao 3 sao do tipo de status e geralmente causam efeitos, e nao dano.
-
-tp_hab eletrizar;
-eletrizar.nome = "Eletrizar!";
-eletrizar.tipo = "eletrico";
-eletrizar.dano = 0;
-
-tp_hab petrificar;
-petrificar.nome = "Petrificar";
-petrificar.tipo = "sombrio";
-petrificar.dano = 0;
-
-tp_hab relaxar;
-relaxar.nome = "Relaxar";
-relaxar.tipo = "agua";
-relaxar.dano = 0;
-
-tp_hab tocarBerimbau;
-tocarBerimbau.nome = "Tocar Berimbau";
-tocarBerimbau.tipo = "dancarino";
-tocarBerimbau.dano = 0;
-
-tp_hab gritoDeGuerra;
-gritoDeGuerra.nome = "Grito de Guerra";
-gritoDeGuerra.tipo = "lutador";
-gritoDeGuerra.dano = 0;
-
-tp_hab botarSaia;
-botarSaia.nome = "Botar Saia";
-botarSaia.tipo = "dancarino";
-botarSaia.dano = 0;
-
-tp_hab chorar;
-chorar.nome = "Chorar";
-chorar.tipo = "normal";
-chorar.dano = 0;
-
-tp_hab hibernar;
-hibernar.nome = "Hibernar";
-hibernar.tipo = "normal";
-hibernar.dano = 0;
-
-tp_hab golpeBaixo;
-golpeBaixo.nome = "Golpe Baixo";
-golpeBaixo.tipo = "lutador";
-golpeBaixo.dano = 25;
-
-tp_hab enrolar;
-enrolar.nome = "Enrolar";
-enrolar.tipo = "planta";
-enrolar.dano = 0;
-
-tp_hab festaEmIpanema;
-festaEmIpanema.nome = "Festa em Ipanema";
-festaEmIpanema.tipo = "dancarino";
-festaEmIpanema.dano = 0;
-
-tp_hab noiteChuvosa;
-noiteChuvosa.nome = "Noite Chuvosa";
-noiteChuvosa.tipo = "Sombrio";
-noiteChuvosa.dano = 0;
-
-tp_hab acelerar;
-acelerar.nome = "Acelerar!";
-acelerar.tipo = "eletrico";
-acelerar.dano = 0;
-
-tp_hab boloDeMorango;
-boloDeMorango.nome = "Bolo de Morango";
-boloDeMorango.tipo = "normal";
-boloDeMorango.dano = 0;
-
-tp_hab sorrirEAcenar;
-sorrirEAcenar.nome = "Sorrir e Acenar";
-sorriEAcenar.tipo = "dancarino";
-sorrirEAcenar.dano = 0;
-
-tp_hab camuflagem;
-camuflagem.nome = "Camuflagem";
-camuflagem.tipo = "planta";
-camuflagem.dano = 0;
-
-
-// HABILIDADE 4
-// habilidades na posicao 4 sao habilidades especiais, unicas de cada passaro e com efeitos especiais
-
-//Bombardeio eletrico
-tp_hab bombardeioEletrico;
-bombardeioEletrico.nome = "Bombardeio Eletrico";
-bombardeioEletrico.tipo = "eletrico";
-bombardeioEletrico.dano = 80;
-
-//Paralizar
-tp_hab paralizar
-paralizar.nome = "Paralizar";
-paralizar.tipo = "sombrio";
-paralizar.dano = 50;
-// paralizar.efeitoNoOponente = Chance do oponente nao atacar no prox turno;
-
-//agua quente
-tp_hab aguaQuente
-aguaQuente.nome = "Agua Quente";
-aguaQuente.tipo = "agua";
-aguaQuente.dano = 55;
-// aguaQuente.efeitoNoOponente = Chance de causar queimadura
-
-//meia-lua
-tp_hab meiaLua;
-meiaLua.nome = "Agua Quente";
-aguaQuente.tipo = "agua";
-aguaQuente.dano = 55;
-
-//avancarComEscudo
-tp_hab avancarComEscudo
-avancarComEscudo.nome = "Avancar com Escudo";
-avancarComEscudo.tipo = "lutador";
-avancarComEscudo.dano = 60;
-//avancarComEscudo.efeitoNoUsuario = aumenta def
-
-//Pintinho amarelinho
-tp_hab pintinhoAmarelinho;
-pintinhoAmarelinho.nome = "Pintinho Amarelinho";
-pintinhoAmarelinho.tipo = "dancarino";
-pintinhoAmarelinho.dano = 0;
-//pintinhoAmarelinho.efeitoNoUsuario = aumenta agi
-//pintinhoAmarelinho.efeitoNoOponente = diminui agi 
-
-
-//Tacada
-tp_hab tacada;
-tacada.nome = "Tacada";
-tacada.tipo = "normal";
-tacada.dano = 60;
-//tacada.efeitoNoOponente = chance de causar confusão
-
-//Extinção
-tp_hab extincao;
-extincao.nome = "Extincao";
-extincao.tipo = "normal";
-extincao.dano = 999;
-//elimina o oponente e a si mesmo instantaneamente
-
-//Rolada
-tp_hab rolada;
-rolada.nome = "Rolada";
-rolada.tipo = "normal";
-rolada.dano = 65;
-//rolada.efeitoNoUsuario = aumenta agi
-
-//Ta dando onda
-tp_hab taDandoOnda;
-taDandoOnda.nome = "Ta Dando Onda";
-taDandoOnda.tipo = "planta";
-taDandoOnda.dano = 55;
-//taDandoOnda.efeitoNoOponente = causa confusão
-
-//Carnaval
-tp_hab carnaval;
-carnaval.nome = "Carnaval"
-carnaval.tipo = "dancarino";
-carnaval.dano = 85;
-//carnaval.efeitoNoOponente = causa confusão
-//carnaval.efeitoNoUsuario = causa confusão
-
-//evanescence
-tp_hab evanescence;
-tp.nome = "Evanescence";
-evanescence.tipo = "sombrio";
-evanescence.dano = 35;
-//evanescence.efeitoNoOponente = diminui def
-
-//Beep Beep
-tp_hab beepBeep;
-beepBeep.nome = "Beep Beep!";
-eLaVamosNos.tipo = "planta";
-beepBeep.dano = 50;
-
-//beepBeep.efeitoNoUsuario = chance de atk 2x
-
-//E lá vamos nós
-tp_hab eLaVamosNos;
-eLaVamosNos.nome = "E la vamos nos!";
-eLaVamosNos.tipo = "planta";
-eLaVamosNos.dano = 55;
-
-//eLaVamosNos.efeitoNoUsuario = chance de escolher outra habilidade para utilizar(round em dobro yeepee)
-
-//Boom?
-tp_hab boom;
-boom.dano = "boom?";
-boom.tipo = "agua";
-boom.dano = 90;
-
-//boom.efeitoNoUsuario = -40 hp
-
-
-// Explosão de Kiwi
-
-tp_hab explosaoDeKiwi;
-explosaoDeKiwi.nome = "Explosao de Kiwi";
-explosaoDeKiwi.tipo = "planta";
-explosaoDeKiwi.dano = 60;
 #endif
