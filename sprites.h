d59e2b2d2ac683f27d91e3c55cce265ad4f70fb6
rcezimbra <rafaelcezimbra04@gmail.com>
2024-09-14 20:35:02 -0300

    botao de iniciar jogo 1

--- sprites.h
+++ sprites.h
@@ -4,4 +4,24 @@
 
 //esta biblioteca serve para guardar as imagens dos passaros
 
+
+printf("          xxxxxxxxx                                                  \n");
+printf("         xx    xxxxx                                                 \n");
+printf("        xx         xxx   xxxxxxxxxxxxxx                              \n");
+printf("        x            xx xx            xxxxx                          \n");
+printf("       xx             xxx                  xx                        \n");
+printf("   xxxxx            xxx    zzzzzz            xxx                     \n");
+printf("  xxxxxxxxxxx     xx            zzzzz          xxx                   \n");
+printf("            x    x                  zzzz         xxx                 \n");
+printf("            xxxxx                      zzz       xxxx                \n");
+printf("               xx                        zz       x xx               \n");
+printf("               xx           zzzz zzzzzz   z       x  xx              \n");
+printf("                xx                    zzzzzz     xx   xx             \n");
+printf("                 xx                             xx     xx            \n");
+printf("                  xx                           xx       xx           \n");
+printf("                    xxx                      xxx         xxx         \n");
+printf("                       xxxxxx            xxxx xxxxx        xx        \n");
+printf("                         xx  xxxxxxxxxxxxx        xxxxxx    xxxx     \n");
+printf("                    xxxxxx   x                         xxxxxx  xxx   \n");
+printf("                         xxxxx                              xxxxxx   \n");
 #endif
