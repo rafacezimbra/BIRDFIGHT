61efd9fdfa5b77c89228ade85c56aaf965320bf7
yasminqjs <yasminqjs@gmail.com>
2024-11-08 12:44:29 -0300

    Update loja.h

--- loja.h
+++ loja.h
@@ -3,34 +3,111 @@
 #include <stdio.h>
 #include <string.h>
 #include "inventario.h"
+#include "apagartela.h"
 #define MAX 100
-//teoricamente é p funcionar
-// Estrutura de item da loja
+
 typedef struct {
     char nome[50];
     int preco;
     char descricao[100];
 } ItemLoja;
 
-// Função para exibir a loja e permitir a compra de itens
-void loja(Inventory *inv, int *BirdCoin) {
+void loja(Inventory *inv, int *BirdCoin){
+apagarTela();
+
     ItemLoja itens[6] = {
         {"Alpiste", 250, "Restaura 30 HP do Birdmon ativo."},
-        {"Gaiola", 150, "Captura o Birdmon adversário"},
+        {"Gaiola", 150, "Captura o Birdmon adversario"},
         {"Melao", 300, "Aumenta o ataque do Birdmon ativo temporariamente."},
         {"Capacete", 300, "Aumenta a defesa do Birdmon ativo temporariamente."},
         {"Biscoito", 500, "Revive um Birdmon com metade do HP."},
         {"Soro", 300, "Retira status negativos do Birdmon ativo."}
     };
 
-    int escolha;
-    printf("\nBem-vindo a Loja!\n");
-    printf("Você tem %d BirdCoins.\n", *BirdCoin);
+printf("\n");
 
-    for (int i = 0; i < 6; i++) {
-        printf("%d - %s (Preço: %d BirdCoins): %s\n", i + 1, itens[i].nome, itens[i].preco, itens[i].descricao);
-    }
-    printf("Escolha um item para comprar (ou 0 para sair): ");
+int escolha;
+printf("|| ***      ******   ********   ****   ||\n");
+printf("|| ***     **    **     **    ***  *** ||\n");
+printf("|| ***     *      *     **    **    ** ||\n");
+printf("|| ***     *      *  *  **    ******** ||\n");
+printf("|| ******  **    **  *  **    **    ** ||\n");
+printf("|| ******   ******   *****    **    ** ||\n");
+printf("Bem-vindo a Loja!\n");
+printf("Voce tem %d BirdCoins.\n", *BirdCoin);
+
+printf("\n");
+for (int i = 0; i < 6; i++) {
+
+switch(i){
+case 0:
+printf("|-------------| \n");
+printf("|   ALPISTE   | \n");
+printf("|-------------| \n");
+printf("|:::::::::::::|     %d - %s (Preco: %d BirdCoins): %s\n", i + 1, itens[i].nome, itens[i].preco, itens[i].descricao);
+printf("|:::::::::::::| \n");
+printf("|:::::::::::::| \n");
+printf("|-------------| \n\n");
+break;
+
+
+//melão
+case 1: 
+printf("          --MM \n");
+printf("      MM##MM:: \n");
+printf("    MMMM####++ \n");
+printf("    ####MM@@++      %d - %s (Preco: %d BirdCoins): %s\n", i + 1, itens[i].nome, itens[i].preco, itens[i].descricao);
+printf("  MM##MM##  ## \n");
+printf("@@--      MM   \n"); 
+printf("  ########     \n\n");    
+break;
+
+//gaiola
+case 2:
+printf("xxxxxxxxxxxxx \n");
+printf("x  x  x  x  x \n");
+printf("x  x  x  x  x \n");
+printf("x  x  x  x  x       %d - %s (Preco: %d BirdCoins): %s\n", i + 1, itens[i].nome, itens[i].preco, itens[i].descricao);
+printf("x  x  x  x  x \n");
+printf("x  x  x  x  x \n");
+printf("xxxxxxxxxxxxx \n\n");
+break;
+
+//soro
+case 3:
+printf("  ___   \n");
+printf(" |   |  \n");
+printf(" _| |_  \n");
+printf("|_____|             %d - %s (Preco: %d BirdCoins): %s\n", i + 1, itens[i].nome, itens[i].preco, itens[i].descricao);
+printf("|_____| \n");
+printf("|_____| \n\n");
+break;
+
+//biscoito
+case 4:
+printf("    *******       \n");
+printf("  ***  @   ****   \n");
+printf(" ** @    @   ***  \n");
+printf(" *    @    @  **    %d - %s (Preco: %d BirdCoins): %s\n", i + 1, itens[i].nome, itens[i].preco, itens[i].descricao);
+printf(" *  @        ***  \n");
+printf(" **     @   ***   \n");
+printf("   **********     \n\n");
+break;
+
+//capacete
+case 5:
+printf(" #######  \n");
+printf("##      # \n"); 
+printf("#    ####           %d - %s (Preco: %d BirdCoins): %s\n", i + 1, itens[i].nome, itens[i].preco, itens[i].descricao);
+printf("#   ##  | \n");
+printf("#   #___| \n");
+printf(" ####     \n\n");
+break;
+
+}
+}
+
+printf("\nEscolha um item para comprar (ou 0 para sair): ");
     scanf("%d", &escolha);
 
     while (escolha != 0) {
@@ -44,7 +121,7 @@ void loja(Inventory *inv, int *BirdCoin) {
                 printf("BirdCoins insuficientes para comprar este item.\n");
             }
         } else {
-            printf("Escolha inválida. Tente novamente.\n");
+            printf("Escolha invalida. Tente novamente.\n");
         }
 
         printf("\nEscolha outro item para comprar ou 0 para sair: ");
