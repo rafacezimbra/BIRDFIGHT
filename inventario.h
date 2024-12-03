0f5dae105718818fd06bd84737849b09f6273bcc
rcezimbra <rafaelcezimbra04@gmail.com>
2024-12-03 19:22:05 -0300

    inventario update 2

--- inventario.h
+++ inventario.h
@@ -101,31 +101,31 @@ int temItem(int numItem, Inventory *inv) { // Checa se o jogador tem pelo menos 
 }
 
 int aplicarEfeito(int itemIndex, tp_pokemon *poke, Inventory *inv) { // Aplica o efeito de um item no inventário
-    if (temItem(itemIndex, inv)) { // Testa se o jogador tem o item antes de aplicar os efeitos
-        if (itemIndex == 0) { // Alpiste
+    if (temItem(itemIndex-1, inv)) { // Testa se o jogador tem o item antes de aplicar os efeitos
+        if (itemIndex == 1) { // Alpiste
             int hpRecovery = 30;
             poke->vida += hpRecovery;
             printf("HP recuperado em %d pontos. HP atual: %d\n", hpRecovery, poke->vida);
             return 1;
-        } else if (itemIndex == 1) { // Melão
+        } else if (itemIndex == 2) { // Melão
             float atkIncreasePercent = 0.2; // Aumento de ataque de 20%
             int atkIncrease = poke->atq * atkIncreasePercent;
             poke->atq += atkIncrease;
             printf("Ataque aumentado em %d pontos. Ataque atual: %d\n", atkIncrease, poke->atq);
             return 1;
-        } else if (itemIndex == 2) { // Capacete
+        } else if (itemIndex == 3) { // Capacete
             float defIncreasePercent = 0.2; // Aumento de defesa de 20%
             int defIncrease = poke->def * defIncreasePercent;
             poke->def += defIncrease; // Corrigido para aumentar a defesa
             printf("Defesa aumentada em %d pontos. Defesa atual: %d\n", defIncrease, poke->def);
             return 1;
-        } else if (itemIndex == 3) {
+        } else if (itemIndex == 4) {
             resetarAtributos(poke);
             return 1;
-        } else if (itemIndex == 4) {
+        } else if (itemIndex == 5) {
             reviverBirdmon(poke);
             return 1;
-        } else if (itemIndex == 5) {
+        } else if (itemIndex == 6) {
             printf("Esse item eh utilizado a partir do menu.\n");
             return 0;
         } else {
